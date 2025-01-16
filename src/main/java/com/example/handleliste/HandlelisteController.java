ec3a0a9b64b07e1fab570e9f230f218adde5da46
tominezak <143406731+tominezak@users.noreply.github.com>
2025-01-16 14:29:59 +0100

    Oppdatert filer

--- src/main/java/com/example/handleliste/HandlelisteController.java
+++ src/main/java/com/example/handleliste/HandlelisteController.java
@@ -1,4 +1,16 @@
 package com.example.handleliste;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HandlelisteController {
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private JdbcTemplate db;
+
 }
