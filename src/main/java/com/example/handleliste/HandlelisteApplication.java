52b4a9e656ae6d05748b5968be63471388bfbb60
tominezak <143406731+tominezak@users.noreply.github.com>
2025-01-16 12:13:30 +0100

    handleliste prosjekt

---
+++ src/main/java/com/example/handleliste/HandlelisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.handleliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlelisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandlelisteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/handleliste/HandlelisteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handleliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlelisteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
